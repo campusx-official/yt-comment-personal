db07ee97184ec161dd794098d41c15023aae7b7b
nitishksingh24 <nitishksingh24@gmail.com>
2024-11-11 10:28:40 +0530

    change appspec.yml

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# Log everything to start_docker.log
+exec > /home/ubuntu/start_docker.log 2>&1
+
 echo "Logging in to ECR..."
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 390844758498.dkr.ecr.us-east-1.amazonaws.com
 
